47c6b4f91318b5ddc43132d5d5cb72b12de2995f
RoshanJose7 <roshanstark007@gmail.com>
2020-10-07 10:13:44 +0530

    Rem: Build

--- server/server.js
+++ server/server.js
@@ -4,16 +4,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-const port = process.env.PORT || 3001;
+const port = 3001;
 
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('build'));
 app.use(express.json());
-
-app.get('/', (req, res) => {
-	res.sendFile('index.html');
-});
 
 app.post('/api/formdata', (req, res) => {
 	const formInput = new ContactForm({
--- startWebApp.sh
+++ startWebApp.sh
@@ -1,1 +1,2 @@
+cd ~/farm-tech-blog/client/ && npm install && pm2 start --name client npm -- start
 cd ~/farm-tech-blog/server/ && npm install && pm2 start server.js
